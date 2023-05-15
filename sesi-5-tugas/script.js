3de7864bbf07b5b4ffd79c10e2805ef63f6f7295
orsix <106032302+ahmadan0311@users.noreply.github.com>
2023-05-15 21:40:26 +0700

    Add files via upload

--- sesi-5-tugas/script.js
+++ sesi-5-tugas/script.js
@@ -1,13 +1,43 @@
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let repeatPassword = document.getElementById('repeatPassword');
 let usernameInput = document.getElementById('usernameInput');
 let passwordInput = document.getElementById('passwordInput');
 
 
-function onLogin() {
-  valid(usernameInput.value)
+function onRegister() {
+  if (valid(username.value) && valid(password.value)) {
+    console.log(username.value, password.value);
 
-  if (valid(usernameInput.value) && valid(passwordInput.value)) {
-    location.replace('admin.html')
+    if (password.value == repeatPassword.value) {
+      localStorage.setItem( username.value, password.value)
+
+      location.replace('index.html')
+    }
+    else {
+      alert('The passwords you entered do not match.')
+    }
   }
+  else {
+    alert('Fill the empty blank');
+  }
+}
+
+function onLogin() {
+  // valid(usernameInput.value)
+
+  if (!valid(usernameInput.value) || !valid(passwordInput.value)) {
+    alert('Fill the empty blank');
+  } else if (localStorage.getItem(usernameInput.value) == passwordInput.value) {
+    location.replace('admin.html')
+  } else if (localStorage.getItem(usernameInput.value) == null){
+    alert('User Not Found');
+  } else if (localStorage.getItem(usernameInput.value) != passwordInput.value) {
+    alert('Password Incorrect');
+  } else {
+    alert('Something is wrong');
+  }
+
 }
 
 function valid(param) {
